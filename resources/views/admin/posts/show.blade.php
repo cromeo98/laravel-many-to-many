8bea5b05d344f3d1e855722e644a3802d53bc083
cromeo98 <romeo.christian98@gmail.com>
2021-09-20 08:30:36 +0200

    add ctageory in show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,8 +4,16 @@
     <div class="card">
         <div class="card-body">
             <h2>Dettagli Post</h2>
-            <h5 class="card-title">{{$post->title}}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">{{$post->slug}}</h6>
+            <h5 class="card-title">Titolo: {{$post->title}}</h5>
+            <div class="card-subtitle mb-2 text-muted">                    
+                @if ($post->category_id)
+                    <h3>Categoria: {{$post->category->name}}</h3>
+                @else
+                    <p>Nessuna categoria associata al post, 
+                        <a href="{{route('admin.posts.edit', $post->id)}}">aggiungila ora.</a>
+                    </p>
+                @endif
+            </div>
             <p class="card-text">{{$post->content}}</p>
             <a href="{{route('admin.posts.index')}}" class="btn btn-primary">Torna alla lista dei post</a>
         </div>
