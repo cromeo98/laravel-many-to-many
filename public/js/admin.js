5dd2837bb63bbd7e780709ca004c3ceaf383aa4e
cromeo98 <romeo.christian98@gmail.com>
2021-09-20 15:22:12 +0200

    add relation method in model tag & post

--- public/js/admin.js
+++ public/js/admin.js
@@ -37689,7 +37689,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\ADMIN\Documents\Boolean\Esercitazioni\Esercizi\mamp_public\laravel-relations\resources\js\admin.js */"./resources/js/admin.js");
+module.exports = __webpack_require__(/*! C:\Users\ADMIN\Documents\Boolean\Esercitazioni\Esercizi\mamp_public\laravel-many-to-many\resources\js\admin.js */"./resources/js/admin.js");
 
 
 /***/ })
